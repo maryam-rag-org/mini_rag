1a4eff9dcb796025ad7acba6a601ca05a5326cb8
MaryamMaksour <maryammaksour@gmail.com>
2026-07-15 00:31:30 +0400

    create QwenProvider

---
+++ src/stores/llm/providers/QwenProvider.py
@@ -0,0 +1,122 @@
+from ..LLMInterface import LLMInterface
+from ..LLMEnums import QwenAIEnums
+from openai import OpenAI
+import logging
+
+
+
+class QwenProvider(LLMInterface):
+
+    def __init__(self, api_key: str, base_url: str,
+                        default_input_max_characters: int=1000,
+                        default_generation_max_output_tokens: int=1000,
+                        default_generation_temperature: float=0.1):
+        
+        self.api_key = api_key
+        self.base_url = base_url
+        self.default_input_max_characters = default_input_max_characters
+        self.default_generation_max_output_tokens = default_generation_max_output_tokens
+        self.default_generation_temperature = default_generation_temperature
+
+        self.generation_model_id = None
+        
+        self.embedding_model_id = None
+        self.embedding_size = None 
+
+        self.client = OpenAI(
+            api_key = self.api_key,
+            base_url = self.base_url
+        )
+ 
+        self.logger = logging.getLogger(__name__)
+
+    def set_generation_model(self, model_id:str): 
+        self.generation_model_id = model_id
+
+    def set_embedding_model(self, model_id:str, embedding_size:int):
+        self.embedding_model_id = model_id
+        self.embedding_size = embedding_size
+
+    def process_text(self, text:str): # hekper methode that is why it is not in the interface
+        
+        text = " ".join(text.strip().split())
+        return text[:self.default_input_max_characters]
+
+
+    def generate_text(self, prompt: str, chat_history: list=[],
+                            max_output_tokens: int = None,
+                            temperature: float = None ):
+        
+        #raise NotImplementedError # For now We do not want to implement this function
+        
+        ###### Validation ######
+        if not self.client:
+            self.logger.error("Qwen client was not set")
+            return None 
+        
+        if not self.embedding_model_id:
+            self.logger.error("Generation model for Qwen was not set")
+            return None 
+        
+        max_output_tokens = max_output_tokens if max_output_tokens else self.default_generation_max_output_tokens
+        temperature = temperature if temperature else self.default_generation_temperature
+
+        #########################
+
+        chat_history.append( self.construct_prompt(prompt=prompt, role=QwenAIEnums.USER.value) )
+
+        response = self.client.chat.completions.create(
+            model= self.generation_model_id,
+            messages=chat_history,
+            max_tokens=max_output_tokens,
+            temperature=temperature
+        )
+
+        ###### Response Validation ######
+        if not response or not response.choices or len(response.choices) == 0 or not response.choices[0].message:
+            self.logger.error("Error while generating text with Qwen")
+            return None
+        
+
+        return response.choices[0].message["content"]
+
+
+
+        
+    def embed_text(self, text:str, document_type:str = None):
+        
+        
+        ###### Validation ######
+        if not self.client:
+            self.logger.error("Qwen client was not set")
+            return None 
+        
+        if not self.embedding_model_id:
+            self.logger.error("Embedding model for Qwen was not set")
+            return None 
+        
+        #########################
+
+        response = self.client.embeddings.create(
+            model=self.embedding_model_id,
+            input= text,
+        )
+        
+        ###### Response Validation ######
+        if not response or not response.data or len(response.data) == 0 or not response.data[0].embedding:
+            self.logger.error("Error while embedding text with Qwene")
+            return None
+        
+
+        return response.data[0].embedding
+
+
+    def construct_prompt(self, prompt:str, role:str):  
+        
+        return {
+            "role": role,
+            "content": self.process_text(prompt)
+        }
+
+
+
