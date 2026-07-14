7b84e000d30aede3fb5244095faaf91882d80dd6
MaryamMaksour <maryammaksour@gmail.com>
2026-07-15 00:30:58 +0400

    create LLMEnums

---
+++ src/stores/llm/LLMEnums.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+
+class LLMEnums(Enum):
+
+    Qwen = "Qwen"
+    COHERE = "COHERE"
+
+class QwenAIEnums(Enum):
+
+    SYSTEM = "system"
+    USER = "usre"
+    ASSISTANT = "assistant"
+
+
