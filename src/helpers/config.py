3fdb12fa330db99e9b68a8e3c2cff9dc2bb80ff6
MaryamMaksour <maryammaksour@gmail.com>
2026-07-15 22:11:46 +0400

    update

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -13,6 +13,23 @@ class Settings(BaseSettings):
     MONGO_URL: str
     MONGODB_DATABASE: str
 
+    GENERATION_BACKEND: str
+    EMBEDDING_BACKEND: str
+
+    QWEN_API_KEY: str = None
+    QWEN_API_URL: str = None
+    
+    COHERE_API_KEY: str = None
+
+    GENERATION_MODEL_ID: str = None
+    EMBEDDING_MODEL_ID: str = None
+    EMBEDDING_MODEL_SIZE: int = None
+
+    INPUT_DEFAULT_MAX_CHARACTERS: int = None
+    GENERATION_DEFAULT_MAX_TOKENS: int = None
+    GENERATION_DEFAULT_TEMPERATURE: float = None
+
+
     class Config:
         env_file = ".env"
 
--- src/stores/llm/LLMEnums.py
+++ src/stores/llm/LLMEnums.py
@@ -3,13 +3,26 @@ from enum import Enum
 
 class LLMEnums(Enum):
 
-    Qwen = "Qwen"
+    Qwen = "QWEN"
     COHERE = "COHERE"
 
-class QwenAIEnums(Enum):
+class QwenEnums(Enum):
 
     SYSTEM = "system"
     USER = "usre"
     ASSISTANT = "assistant"
 
+class CoHereEnums(Enum):
+
+    SYSTEM = "SYSTEM"
+    USER = "USER"
+    ASSISTANT = "CHATBOT"
+
+    DOCUMENT = "search_document"
+    QUERY = "search_query"
+
+
+class DocumentTypeEnum(Enum):
+    DOCUMENT = "document"
+    QUERY = "query"
 
