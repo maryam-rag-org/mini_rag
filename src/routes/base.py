1b71ada3eea18716acd2a356ade5e4b065af1693
Github <github_user@ip-172-31-37-65.eu-north-1.compute.internal>
2026-08-13 13:47:03 +0000

    add datetime to route /

--- src/routes/base.py
+++ src/routes/base.py
@@ -3,6 +3,7 @@ from helpers.config import get_settings, Settings
 
 import os
 
+from datatime import datatime
 
 base_router = APIRouter(
     prefix="/api/v1",
@@ -23,4 +24,5 @@ async def welcome_message(app_settings: Settings = Depends(get_settings)):
     return {
         "app_name": app_name,
         "app_version": app_version,
+        "datatime": datatime.now().strftime("%Y-%m-%d %H:%M:%S")
     }
